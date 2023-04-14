f2af5eacc18816f9edecc5815887f5d46a599f67
biz-cloud-abhishek <120293411+abhishekBCE@users.noreply.github.com>
2023-04-13 20:47:08 -0500

    lambdaLayer added

--- src/mach1-mcleod.js
+++ src/mach1-mcleod.js
@@ -1,3 +1,38 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({
+    region: 'us-east-1'
+});
+const csv = require('@fast-csv/parse');
+
 module.exports.handler = async (event, context) => {
     console.log("EVENT:", event);
+    
+    let eventBody = JSON.parse(event.Records[0].body);
+    let bucketName = eventBody.Records[0].s3.bucket.name;
+    let objectName = eventBody.Records[0].s3.object.key;
+ 
+
+    const params = {
+        Bucket: bucketName,
+        Key: objectName,
+      };
+    const s3Stream = s3.getObject(params).createReadStream();
+
+    let resultArr = await parseCSVData(s3Stream);
+    console.log("resultArr:", JSON.stringify(resultArr));
+}
+
+
+async function parseCSVData(s3Stream) {
+    return new Promise( (resolve, reject) => {
+        let result = [];
+        csv
+            .parseStream(s3Stream)
+            .on("data", (data) => {
+                result.push(data);
+            })
+            .on("end", () => {
+                resolve( result )
+            });
+    });
 }

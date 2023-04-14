81b4372785df63b50ee628e925a31756a6918829
biz-cloud-abhishek <120293411+abhishekBCE@users.noreply.github.com>
2023-04-14 11:04:05 -0500

    logic changed for reading csv

--- src/mach1-mcleod.js
+++ src/mach1-mcleod.js
@@ -2,7 +2,14 @@ const AWS = require('aws-sdk');
 const s3 = new AWS.S3({
     region: 'us-east-1'
 });
-const csv = require('@fast-csv/parse');
+// const csv = require('@fast-csv/parse');
+const csv = require('csvtojson');
+
+const csv_headers = ["source_system","CONSOL_NBR","ORIGIN_PORT","ORIGIN_CITY","ORIGIN_ST","ORIGIN_LOC_ID",
+    "DESTINATION_PORT","DESTINATION_CITY","DESTINATION_ST","DESTINATION_LOC_ID","carrier","CARRIER_BOOKING_RREF",
+    "AGENT_REF","BOL","OTHER_INFO","TRUCK_REF","AGENT_TYPE","TRANSPORT_MODE","CONSOL_MODE","ETD","ETA","ATD","ATA",
+    "SHIP_COUNT","PACKS","ACTUAL_WEIGHT","CHARGEABLE_WEIGHT","UW"
+] 
 
 module.exports.handler = async (event, context) => {
     console.log("EVENT:", event);
@@ -18,7 +25,7 @@ module.exports.handler = async (event, context) => {
       };
     const s3Stream = s3.getObject(params).createReadStream();
 
-    let resultArr = await parseCSVData(s3Stream);
+    let resultArr = await csv({headers : csv_headers}).fromStream(s3Stream);
     console.log("resultArr:", JSON.stringify(resultArr));
 }
 
